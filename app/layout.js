85f4ccadc9d61737c5b96d82a85423bb8fa51da6
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2023-08-09 16:57:51 -0400

    test4

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,21 @@
 'use client';
 import './globals.css'
-
-if(typeof document !== 'undefined') {
-  (function(d, t) {
-    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-    v.onload = function() {
-      window.voiceflow.chat.load({
-          verify: { projectID: '64d2d37526dfd90007fa04bd' },
-          url: 'https://general-runtime.voiceflow.com',
-          versionID: 'production'
-      });
-    }
-    v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
-  })(document, 'script');
-}
-
+import {useEffect} from "react";
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+            verify: { projectID: '64d2d37526dfd90007fa04bd' },
+            url: 'https://general-runtime.voiceflow.com',
+            versionID: 'production'
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+    })(document, 'script');
+  }, []);
   return (
     <html lang="en">
       <head>
